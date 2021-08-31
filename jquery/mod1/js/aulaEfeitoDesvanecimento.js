5ddafdcd9781c7624ae6466591b0bf8f34221aac
Mateusads <47679269+Mateusads@users.noreply.github.com>
2021-08-31 17:14:40 -0300

    Versão final 1.0 do projeto usando todas as aulas

--- jquery/mod1/js/aulaEfeitoDesvanecimento.js
+++ jquery/mod1/js/aulaEfeitoDesvanecimento.js
@@ -2,6 +2,7 @@ $(function () {
 
     var ex1 = $('.ex1');
     var ex2= $('.ex2');
+    var ex3= $('.ex3');
     var button = $(':button');
 
 /*     button.click(function(){
@@ -15,9 +16,19 @@ $(function () {
         ex1.fadeToggle("slow");
     }); */
 
-    button.click(function(){
+/*     button.click(function(){
         ex1.fadeTo(3000 ,0.6, function(){
             ex2.fadeTo(1000, 0.1);
+        });
+
+    });
+ */
+
+    button.click(function(){
+        ex1.fadeToggle(4000 ,function(){
+            ex3.fadeToggle(3000, function(){
+                ex2.fadeToggle(2000);
+            });
         });
 
     });
--- jquery/mod1/js/aulaMenuFixo.js
+++ jquery/mod1/js/aulaMenuFixo.js
@@ -3,7 +3,7 @@ $(function () {
     var nav = $('.menu');
 
     $(window).scroll(function(){
-        if($(this).scrollTop() > 53){
+        if($(this).scrollTop() > 193){
             nav.addClass("menu-fixo");
         }else{
             nav.removeClass("menu-fixo");
